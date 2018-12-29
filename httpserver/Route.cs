282294dbb27b50e99747d1826da0baeedfb2dd82
Jonathan C <jchiang@8thlight.com>
2018-12-29 12:36:08 -0600

    removing type variant indicators from Func

--- httpserver/Route.cs
+++ httpserver/Route.cs
@@ -5,9 +5,9 @@ namespace Server
     {
         private string _method;
         private string _path;
-        private readonly Func<in IRequest, in IResponse, out IResponse> _controller;
+        private readonly Func<IRequest, IResponse, IResponse> _controller;
 
-        public Route(string method, string path, Func<in IRequest, in IResponse, out IResponse> controller)
+        public Route(string method, string path, Func<IRequest, IResponse, IResponse> controller)
         {
             _method = method;
             _path = path;
