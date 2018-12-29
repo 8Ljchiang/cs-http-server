69e490c72ac253c5d253dfcd1b207662d85455da
Jonathan C <jchiang@8thlight.com>
2018-12-29 10:18:54 -0600

    added Route class

--- httpserver/Route.cs
+++ httpserver/Route.cs
@@ -3,8 +3,19 @@ namespace Server
 {
     public class Route
     {
-        public Route()
+        private string _method;
+        private string _path;
+        private string _controllerId;
+
+        public Route(string method, string path, string controllerId)
         {
+            _method = method;
+            _path = path;
+            _controllerId = controllerId;
         }
+
+        public string ControllerId { get => _controllerId; set => _controllerId = value; }
+        public string Path { get => _path; set => _path = value; }
+        public string Method { get => _method; set => _method = value; }
     }
 }
