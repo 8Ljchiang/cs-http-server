80d6a70385cf7ba86b08635052c42eec449d41c6
Jonathan C <jchiang@8thlight.com>
2018-12-30 16:54:31 -0600

    added CreateResponseString test with body text

--- httpserver.Tests/ResponseBuilderTests.cs
+++ httpserver.Tests/ResponseBuilderTests.cs
@@ -26,7 +26,22 @@ namespace Server.UnitTests
         [Fact]
         public void CreateResponseString_CalledWithValidResponseWithBodyText_ReturnsString()
         {
+            // Arrange
+            string body = "{ \"response\": \"hello world\" }";
+            Response response = new Response();
+            response.AddHeader("Access", "*/*");
+            response.Body = body;
+            string expectedResponseString =
+                "HTTP/1.1 200 OK\n" +
+                "Access: */*\n" +
+                "Content-Type: text/html\n" +
+                "\n" + body;
 
+            // Act
+            string actualResponseString = ResponseBuilder.CreateResponseString(response);
+
+            // Assert
+            Assert.Equal(expectedResponseString, actualResponseString);
         }
     }
 }
