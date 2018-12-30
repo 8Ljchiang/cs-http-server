713b0797967fa5c15c4eea6ebafe63bc2c445bde
Jonathan C <jchiang@8thlight.com>
2018-12-30 16:36:10 -0600

    created RequestBuilder class file

---
+++ httpserver/ResponseBuilder.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace Server
+{
+    public static class ResponseBuilder
+    {
+        public static string CreateResponseString(Response response)
+        {
+            return "";
+        }
+    }
+}
