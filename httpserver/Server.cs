e856dc72ee24ca77aa9477dd1f52bd5407a612a5
Jonathan C <jchiang@8thlight.com>
2018-12-31 10:22:05 -0600

    created Server classes

---
+++ httpserver/Server.cs
@@ -0,0 +1,36 @@
+﻿using System;
+namespace Server
+{
+    public class Server
+    {
+        private Router _router;
+
+        public Server(Router router)
+        {
+            _router = router;
+        }
+
+        public void Listen(int port)
+        {
+            // 1. Initialize Listener
+
+            // 2. Accept Incoming Conncetions
+
+            // 3. Get data from Connection.
+
+            // 4. Convert data to string.
+
+            // 5. Convert string to Request Object.
+
+            // 6. Process Request object and Build response object.
+
+            // 7. Convert Response object to string.
+
+            // 8. Convert response string to data.
+
+            // 9. Send data to connection.
+
+            // 10. Close connections
+        }
+    }
+}
