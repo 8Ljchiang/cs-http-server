f971c7904148d4c96c18fe9a5f96fc20fd2b2585
Jonathan C <jchiang@8thlight.com>
2018-12-31 17:02:35 -0600

    created CommandDispatcher class

---
+++ httpserver.Tests/CommandDispatcherTests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using Xunit;
+
+namespace Server.UnitTests
+{
+    public class CommandDispatcherTests
+    {
+        [Fact]
+        public void RegisterCommandHandler_CalledOnce_AddsHandler()
+        {
+            // Arrange
+            int expectedCommandTypeCount = 1;
+            int expectedHandlerCount = 1;
+            string commandType = "listen";
+            Action<Dictionary<string, object>> handler = (Dictionary<string, object> payload) =>
+            { 
+
+            };
+            CommandDispatcher dispatcher = new CommandDispatcher();
+
+            // Act
+            dispatcher.RegisterCommandHandler(commandType, handler);
+
+            // Assert
+            Assert.Equal(expectedCommandTypeCount, dispatcher.Commands.Count);
+            Assert.Equal(expectedHandlerCount, dispatcher.Commands[commandType].Count);
+        }
+    }
+}
